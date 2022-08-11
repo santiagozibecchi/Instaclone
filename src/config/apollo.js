6852514da00713c1ae1774fc633b62db4af90e0a
Santiago Zibecchi <santiago.zibe@gmail.com>
2022-08-11 20:37:17 -0300

    coneccion cliente/servidor establecida

--- src/config/apollo.js
+++ src/config/apollo.js
@@ -1,7 +1,7 @@
 import { ApolloClient, /* createHttpLink */InMemoryCache } from '@apollo/client';
 import { createUploadLink } from 'apollo-upload-client';
-
-// Coneccion del servidor con el cliente
+import { setContext } from 'apollo-link-context';
+import { getToken } from '../utils/token';
 
 // Codigo anterior .....
 // const httpLink = createHttpLink({
@@ -13,10 +13,24 @@ const httpLink = createUploadLink({
      uri: 'http://localhost:4000/graphql'
 });
 
+// Coneccion del servidor con el cliente
+const authLink = setContext((_, { headers }) => {
+     const token = getToken();
+
+     return {
+          headers: {
+               ...headers, /* Para obtener todos los datos  */
+               Authorization: token ? `Bearer ${token}` : '',
+          },
+     };
+});
+
+// En cada peticion que se haga en la aplicacion si estamos logeado se mandara
+// el token y sino, no se mandara
 const client = new ApolloClient({
      connectToDevTools: true,
      cache: new InMemoryCache(),
-     link: httpLink,
+     link: authLink.concat(httpLink),
 });
 
 
