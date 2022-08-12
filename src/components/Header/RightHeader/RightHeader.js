1741f7d36331e9e35dda82e69d21adee39f0c29c
Santiago Zibecchi <santiago.zibe@gmail.com>
2022-08-12 00:36:05 -0300

    actualizacion mutua con el header y perfil

--- src/components/Header/RightHeader/RightHeader.js
+++ src/components/Header/RightHeader/RightHeader.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Icon, Image } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import { GET_USER } from '../../../gql/user'
 import useAuth from '../../../hooks/useAuth';
 import ImageneNotFound from '../../../assets/png/avatar.png';
 import './RightHeader.scss';
@@ -10,7 +12,12 @@ const RightHeader = () => {
      // para traer la informacion del usuario
      const { auth } = useAuth();
 
+     const { data, loading, error } = useQuery(GET_USER, {
+          variables: { username: auth.username }
+     });
 
+     if (loading || error) return null;
+     const { getUser } = data; /* data es todo el objeto que contiene la data del usuario */
 
      return (
           <div className='right-header'>
@@ -21,7 +28,7 @@ const RightHeader = () => {
                <Icon name='plus' />
 
                <Link to={`/${auth.username}`}>
-                    <Image src={ImageneNotFound} avatar />
+                    <Image src={getUser.avatar ? getUser.avatar : ImageneNotFound} avatar />
                </Link>
           </div>
 
