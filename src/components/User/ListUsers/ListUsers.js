34e143283dd487344dec92b5a00da0f5def86abf
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2022-08-19 20:42:11 -0300

    ir al perfil de seguidores

--- src/components/User/ListUsers/ListUsers.js
+++ src/components/User/ListUsers/ListUsers.js
@@ -2,9 +2,17 @@ import React from "react";
 import { size, map } from "lodash";
 import { Image } from "semantic-ui-react";
 import ImageNoFound from "../../../assets/png/avatar.png";
+import { useHistory } from "react-router-dom";
 import "./ListUsers.scss";
 
 const ListUsers = ({ users, setShowModal }) => {
+   const history = useHistory();
+
+   const goToUser = (username) => {
+      setShowModal(false);
+      history.push(`/${username}`);
+   };
+
    return (
       <div className="list-users">
          {size(users) === 0 ? (
@@ -15,7 +23,7 @@ const ListUsers = ({ users, setShowModal }) => {
             map(users, (user, index) => (
                <div key={index} className="list-users__user">
                   <Image src={user.avatar || ImageNoFound} avatar />
-                  <div>
+                  <div onClick={() => goToUser(user.username)}>
                      <p>{user.name}</p>
                      <p>{user.username}</p>
                   </div>
