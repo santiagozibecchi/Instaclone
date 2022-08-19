5cfba0df4774ae41a5e92b0764d187b80b857cc5
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2022-08-19 11:33:15 -0300

    seguir y dejar de seguir user

--- src/components/User/Profile/HeaderProfile/HeaderProfile.js
+++ src/components/User/Profile/HeaderProfile/HeaderProfile.js
@@ -1,23 +1,38 @@
-import React from 'react';
-import { Button } from 'semantic-ui-react';
-import './HeaderProfile.scss';
+import React from "react";
+import { Button } from "semantic-ui-react";
+import { useQuery } from "@apollo/client";
+import { IS_FOLLOW } from "../../../../gql/follow";
+import "./HeaderProfile.scss";
 
 const HeaderProfile = (props) => {
+   const { getUser, auth, handlerModal } = props;
+   // console.log(auth);
 
-     const { getUser, auth, handlerModal } = props;
+   const { data, loading } = useQuery(IS_FOLLOW, {
+      variables: { username: getUser.username },
+   });
 
-     // console.log(auth);
+   console.log(data);
 
-     return (
-          <div className='header-profile'>
-               <h2>{getUser.username}</h2>
-               {
-                    auth.username === getUser.username
-                         ? <Button onClick={() => handlerModal('settings')}>Ajustes</Button>
-                         : <Button>Seguir</Button>
-               }
-          </div>
-     )
-}
+   const buttonFollow = () => {
+      if (data.isFollow) {
+         return <Button className="btn-danger">Dejar de seguir</Button>;
+      } else {
+         return <Button className="btn-action">Seguir</Button>;
+      }
+   };
 
-export default HeaderProfile
+   return (
+      <div className="header-profile">
+         <h2>{getUser.username}</h2>
+         {auth.username === getUser.username ? (
+            <Button onClick={() => handlerModal("settings")}>Ajustes</Button>
+         ) : (
+            // Si loading es false, significa que ya ha terminado de cargar la peticion
+            !loading && buttonFollow()
+         )}
+      </div>
+   );
+};
+
+export default HeaderProfile;
