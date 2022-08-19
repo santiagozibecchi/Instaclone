71f18d9a3e4e66e5cd585d4769413f715cd13b8e
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2022-08-19 14:36:40 -0300

    boton seguir usuarios

--- src/components/User/Profile/HeaderProfile/HeaderProfile.js
+++ src/components/User/Profile/HeaderProfile/HeaderProfile.js
@@ -1,24 +1,42 @@
 import React from "react";
 import { Button } from "semantic-ui-react";
-import { useQuery } from "@apollo/client";
-import { IS_FOLLOW } from "../../../../gql/follow";
+import { useQuery, useMutation } from "@apollo/client";
+import { IS_FOLLOW, FOLLOW } from "../../../../gql/follow";
 import "./HeaderProfile.scss";
 
 const HeaderProfile = (props) => {
    const { getUser, auth, handlerModal } = props;
    // console.log(auth);
 
-   const { data, loading } = useQuery(IS_FOLLOW, {
+   const [follow] = useMutation(FOLLOW);
+
+   const { data, loading, refetch } = useQuery(IS_FOLLOW, {
       variables: { username: getUser.username },
    });
-
-   console.log(data);
 
    const buttonFollow = () => {
       if (data.isFollow) {
          return <Button className="btn-danger">Dejar de seguir</Button>;
       } else {
-         return <Button className="btn-action">Seguir</Button>;
+         return (
+            <Button className="btn-action" onClick={onFollow}>
+               Seguir
+            </Button>
+         );
+      }
+   };
+
+   const onFollow = async () => {
+      try {
+         await follow({
+            variables: {
+               username: getUser.username,
+            },
+         });
+
+         refetch();
+      } catch (error) {
+         console.log(error);
       }
    };
 
--- src/gql/follow.js
+++ src/gql/follow.js
@@ -1,7 +1,13 @@
 import { gql } from "@apollo/client";
 
 export const IS_FOLLOW = gql`
-   query isFollow($username: String!){
+   query isFollow($username: String!) {
       isFollow(username: $username)
+   }
+`;
+
+export const FOLLOW = gql`
+   mutation follow($username: String!) {
+      follow(username: $username)
    }
 `;
