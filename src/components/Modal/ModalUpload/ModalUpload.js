0f71e13091240e39273a7798f83b84e12ecf9182
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2022-08-20 20:31:56 -0300

    dropZone agregado al modal +

--- src/components/Modal/ModalUpload/ModalUpload.js
+++ src/components/Modal/ModalUpload/ModalUpload.js
@@ -1,16 +1,44 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { Modal, Icon, Button, Loader } from "semantic-ui-react";
+import { useDropzone } from "react-dropzone";
 import "./ModalUpload.scss";
 
 const ModalUpload = ({ show, setShow }) => {
+   // lugar donde llega la img donde el usuario la sube
+   const onDrop = useCallback((acceptedFile) => {
+
+      const file = acceptedFile[0]
+      console.log(file);
+   });
+
+   // Para poder subir las imagenes
+   const { getRootProps, getInputProps } = useDropzone({
+      accept: {
+         "image/png": [".png"],
+         "image/jpg": [".jpg"],
+      },
+      noKeyboard: true,
+      multiple: false,
+      onDrop,
+   });
+
    // Funcion para cerrar el modal
    const onClose = () => {
       setShow(false);
    };
 
    return (
-      <Modal size="small" open={show} onClose={onClose} className="modal-upload">
-         <h1>Esto es el modal upload</h1>
+      <Modal
+         size="small"
+         open={show}
+         onClose={onClose}
+         className="modal-upload"
+      >
+         <div {...getRootProps()} className="dropzone">
+            <Icon name="cloud upload" />
+            <p>Arrastra tu foto que quieras publicar</p>
+            <input {...getInputProps()} />
+         </div>
       </Modal>
    );
 };
