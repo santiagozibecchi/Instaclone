138738462add03741504978f029bf0247915602d
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2022-08-20 21:05:52 -0300

    mostrar imagen al cliente URL.createObjectURL

--- src/components/Modal/ModalUpload/ModalUpload.js
+++ src/components/Modal/ModalUpload/ModalUpload.js
@@ -1,14 +1,20 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import { Modal, Icon, Button, Loader } from "semantic-ui-react";
 import { useDropzone } from "react-dropzone";
 import "./ModalUpload.scss";
 
 const ModalUpload = ({ show, setShow }) => {
+   // Estado para mostrar la imagen subida al cliente
+   const [fileUpload, setFileUpload] = useState(null);
+
    // lugar donde llega la img donde el usuario la sube
    const onDrop = useCallback((acceptedFile) => {
-
-      const file = acceptedFile[0]
-      console.log(file);
+      const file = acceptedFile[0];
+      setFileUpload({
+         type: "image",
+         file,
+         preview: URL.createObjectURL(file),
+      });
    });
 
    // Para poder subir las imagenes
@@ -28,17 +34,28 @@ const ModalUpload = ({ show, setShow }) => {
    };
 
    return (
-      <Modal
-         size="small"
-         open={show}
-         onClose={onClose}
-         className="modal-upload"
-      >
-         <div {...getRootProps()} className="dropzone">
-            <Icon name="cloud upload" />
-            <p>Arrastra tu foto que quieras publicar</p>
+      <Modal size="tiny" open={show} onClose={onClose} className="modal-upload">
+         <div
+            {...getRootProps()}
+            className="dropzone"
+            style={fileUpload && { border: 0 }}
+         >
+            {!fileUpload && (
+               <>
+                  <Icon name="cloud upload" />
+                  <p>Arrastra tu foto que quieras publicar</p>
+               </>
+            )}
             <input {...getInputProps()} />
          </div>
+         {fileUpload?.type === "image" && (
+            <div
+               className="image"
+               style={{
+                  backgroundImage: `url('${fileUpload.preview}')`,
+               }}
+            />
+         )}
       </Modal>
    );
 };
