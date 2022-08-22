c657989fcbede3a875e5a3c1b04ceda4df4c229b
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2022-08-22 00:37:35 -0300

    componente publication - mostrar publi

--- src/pages/User.js
+++ src/pages/User.js
@@ -4,6 +4,7 @@ import { useQuery } from "@apollo/client";
 import { GET_PUBLICATION } from "../gql/publication";
 import { size } from "lodash";
 import Profile from "../components/User/Profile";
+import Publications from "../components/Publications";
 
 // A esta pagina de user, lo vamos a dividir en dos componentes
 // Componente Profile donde se va a encontrar todos los datos del usuario=> Persona que seguimos, que nos siguen, etc
@@ -26,6 +27,7 @@ const User = () => {
             username={username}
             totalPublications={size(getPublications)}
          />
+         <Publications getPublications={getPublications} />
       </>
    );
 };
