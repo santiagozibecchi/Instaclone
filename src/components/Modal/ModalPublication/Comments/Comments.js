59d2f529333164cabdfea9b612323ba4499376fb
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2022-08-23 11:01:55 -0300

    realtime para los comentarios de una publicacion

--- src/components/Modal/ModalPublication/Comments/Comments.js
+++ src/components/Modal/ModalPublication/Comments/Comments.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Image } from "semantic-ui-react";
 import { map } from "lodash";
 import { Link } from "react-router-dom";
@@ -8,11 +8,23 @@ import { GET_COMMENTS } from "../../../../gql/comment";
 import "./Comments.scss";
 
 const Comments = ({ publication }) => {
-   const { data, loading } = useQuery(GET_COMMENTS, {
+   const { data, loading, startPolling, stopPolling } = useQuery(GET_COMMENTS, {
       variables: {
          idPublication: publication.id,
       },
    });
+
+   // ! El realtime tiene un impacto sobre el servidor pero con las publicaciones varia
+   // ! ya que el realtime solo se va a estar ejecutando cuando el usuario
+   // ! tenga una publicacion abierta
+
+   useEffect(() => {
+      startPolling(1000);
+
+      return () => {
+         stopPolling();
+      };
+   }, [startPolling, stopPolling]);
 
    if (loading) return null;
    const { getComments } = data;
