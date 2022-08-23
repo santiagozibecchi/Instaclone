ccd9199c4780a2d1adbd560653266986b90bafbf
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2022-08-23 09:58:38 -0300

    validacion del formulario de comentarios

--- src/components/Modal/ModalPublication/CommentForm/CommentForm.js
+++ src/components/Modal/ModalPublication/CommentForm/CommentForm.js
@@ -1,11 +1,31 @@
 import React from "react";
 import { Form, Button } from "semantic-ui-react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 import "./CommentForm.scss";
 
 const CommentForm = ({ publication }) => {
+   const formik = useFormik({
+      initialValues: {
+         comment: "",
+      },
+      validationSchema: Yup.object({
+         comment: Yup.string().required(),
+      }),
+      onSubmit: (formData) => {
+         console.log(formData);
+      },
+   });
+
    return (
-      <Form className="comment-form">
-         <Form.Input placeholder="Agrega tu comentario" name="comment" />
+      <Form className="comment-form" onSubmit={formik.handleSubmit}>
+         <Form.Input
+            placeholder="Agrega tu comentario"
+            name="comment"
+            value={formik.values.comment}
+            onChange={formik.handleChange}
+            error={formik.errors.comment && true}
+         />
          <Button type="submit">Publicar</Button>
       </Form>
    );
