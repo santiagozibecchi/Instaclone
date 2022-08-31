4cb62787d46f2cedd3560477a6307fe0ff3b6a70
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2022-08-31 14:49:09 -0300

    Pintado el header del feed de publicaciones

--- src/components/Home/Feed/Feed.js
+++ src/components/Home/Feed/Feed.js
@@ -12,12 +12,21 @@ const Feed = () => {
 
    if (loading) return null;
    const { getPublicationsFolloweds } = data;
+   console.log(getPublicationsFolloweds);
 
    return (
       <div className="feed">
          {map(getPublicationsFolloweds, (publication, index) => (
-            <div key={index}>
-               <p>{publication.file}</p>
+            <div key={index} className="feed__box">
+               <Link to={`/${publication.idUser.username}`}>
+                  <div className="feed__box-user">
+                     <Image
+                        src={(publication.idUser.avatar || ImageNotFound)}
+                        avatar
+                     />
+                     <span>{publication.idUser.name}</span>
+                  </div>
+               </Link>
             </div>
          ))}
       </div>
