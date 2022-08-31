c8db46d72b9b79e2c913f9cca9ea4a54050da70e
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2022-08-31 16:58:54 -0300

    Abriendo las publicaciones del feef

--- src/components/Home/Feed/Feed.js
+++ src/components/Home/Feed/Feed.js
@@ -7,41 +7,60 @@ import { GET_PUBLICATIONS_FOLLOWEDS } from "../../../gql/publication";
 import Action from "../../Modal/ModalPublication/Actions";
 import ImageNotFound from "../../../assets/png/avatar.png";
 import CommentForm from "../../Modal/ModalPublication/CommentForm";
+import ModalPublication from "../../Modal/ModalPublication";
 import "./Feed.scss";
 
 const Feed = () => {
+   const [showModal, setShowModal] = useState(false);
+   const [publicationSelect, setPublicationSelect] = useState(null);
+
    const { data, loading } = useQuery(GET_PUBLICATIONS_FOLLOWEDS);
 
    if (loading) return null;
    const { getPublicationsFolloweds } = data;
-   console.log(getPublicationsFolloweds);
+   // console.log(getPublicationsFolloweds);
+
+   const openPublication = (publication) => {
+      setPublicationSelect(publication);
+      setShowModal(true);
+   };
 
    return (
-      <div className="feed">
-         {map(getPublicationsFolloweds, (publication, index) => (
-            <div key={index} className="feed__box">
-               <Link to={`/${publication.idUser.username}`}>
-                  <div className="feed__box-user">
-                     <Image
-                        src={publication.idUser.avatar || ImageNotFound}
-                        avatar
-                     />
-                     <span>{publication.idUser.name}</span>
+      <>
+         <div className="feed">
+            {map(getPublicationsFolloweds, (publication, index) => (
+               <div key={index} className="feed__box">
+                  <Link to={`/${publication.idUser.username}`}>
+                     <div className="feed__box-user">
+                        <Image
+                           src={publication.idUser.avatar || ImageNotFound}
+                           avatar
+                        />
+                        <span>{publication.idUser.name}</span>
+                     </div>
+                  </Link>
+                  <div
+                     onClick={() => openPublication(publication)}
+                     className="feed__box-photo"
+                     style={{ backgroundImage: `url(${publication.file})` }}
+                  />
+                  <div className="feed__box-actions">
+                     <Action publication={publication} />
                   </div>
-               </Link>
-               <div
-                  className="feed__box-photo"
-                  style={{ backgroundImage: `url(${publication.file})` }}
-               />
-               <div className="feed__box-actions">
-                  <Action publication={publication} />
+                  <div className="feed__box-form">
+                     <CommentForm publication={publication} />
+                  </div>
                </div>
-               <div className="feed__box-form">
-                  <CommentForm publication={publication} />
-               </div>
-            </div>
-         ))}
-      </div>
+            ))}
+         </div>
+         {showModal && (
+            <ModalPublication
+               show={showModal}
+               setShow={setShowModal}
+               publication={publicationSelect}
+            />
+         )}
+      </>
    );
 };
 
